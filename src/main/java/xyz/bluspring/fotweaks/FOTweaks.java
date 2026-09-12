6f480371a183315f130b8f73ecf239751f8bcd55
BluSpring <bluspringindohq@gmail.com>
2026-09-12 13:26:46 +0800

    Add config to disable flies in Spawn

---
+++ src/main/java/xyz/bluspring/fotweaks/FOTweaks.java
@@ -0,0 +1,8 @@
+package xyz.bluspring.fotweaks;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(FOTweaks.MODID)
+public class FOTweaks {
+    public static final String MODID = "fotweaks";
+}
---
+++ src/main/java/xyz/bluspring/fotweaks/mixin/spawn/disable_flies/FlyCapabilityProviderMixin.java
@@ -0,0 +1,38 @@
+package xyz.bluspring.fotweaks.mixin.spawn.disable_flies;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.ninni.spawn.server.gui.fly.FlyCapabilityProvider;
+import com.ninni.spawn.server.gui.fly.FlyData;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.nbt.CompoundTag;
+
+@Pseudo
+@Mixin(FlyCapabilityProvider.class)
+public abstract class FlyCapabilityProviderMixin {
+    @Shadow @Final private List<FlyData> flies;
+
+    @Inject(method = "setFlies", at = @At("HEAD"), cancellable = true)
+    private void avoidSettingFlies(List<FlyData> flies, CallbackInfo ci) {
+        ci.cancel();
+    }
+
+    @Inject(method = "updateFly", at = @At("HEAD"), cancellable = true)
+    private void avoidUpdatingFlies(UUID uuid, CompoundTag tag, CallbackInfo ci) {
+        ci.cancel();
+    }
+
+    @Inject(method = "deserializeNBT", at = @At("HEAD"), cancellable = true)
+    private void avoidDeserializingFlies(CompoundTag tag, CallbackInfo ci) {
+        ci.cancel();
+        this.flies.clear();
+    }
+}
